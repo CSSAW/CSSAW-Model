d3605736e0ad3db8923508c7025415844a41f46c
Brendan <53023770+brendankhoury@users.noreply.github.com>
2020-07-20 15:06:56 -0700

    Made the credentials system marginally more robust

--- Getter Methods/dwa_getter.py
+++ Getter Methods/dwa_getter.py
@@ -5,7 +5,7 @@ from cssaw_central.Session import Session
 # Dates are integers in the YYYYMMDD format 
 # session is an instance of cssaw-central
 # station is a string 
-def get_dwa_data(startDate, endDate, session ,station):
+def get_dwa_data(startDate, endDate, session, station):
     
 
     return None
@@ -13,15 +13,14 @@ def get_dwa_data(startDate, endDate, session ,station):
 
 if __name__ == "__main__":
     credentials = open('../credentials.txt', 'r')
-    username = credentials.readline()[:-1]
-    password = credentials.readline()[:-1]
-    host = credentials.readline()[:]
+    username = credentials.readline().replace('\n','')
+    password = credentials.readline().replace('\n','')
+    host = credentials.readline().replace('\n','')
     print(username,password,host)
     sess = Session(username,password, host, db='CENTRAL')
     startDate = 19830000
     endDate = 19850000
-
     
-    testDf = get_dwa_data()
+    testDf = get_dwa_data(startDate,endDate, sess, 'A2H056')
 
     pass
