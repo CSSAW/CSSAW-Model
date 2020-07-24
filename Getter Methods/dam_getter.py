28c730e2888b968c1045f5a6fbaf65bf7aba413a
Lake Yin <la11010e@gmail.com>
2020-07-24 17:05:48 -0400

    Added comments

--- Getter Methods/dam_getter.py
+++ Getter Methods/dam_getter.py
@@ -2,6 +2,7 @@ import pandas as pd
 from cssaw_central.Session import Session
 
 def get_dam_levels(startDate, endDate, session, province, river):
+    # lookup table of provinces
     provinces = {"EC":"EasternCape",
                 "FS":"FreeState",
                 "G":"Gauteng",
@@ -12,12 +13,15 @@ def get_dam_levels(startDate, endDate, session, province, river):
                 "NW":"NorthWest",
                 "WC":"WesternCape"}
 
+    # check that the province exists in the dictionary
     if provinces.get(province) == None and province not in provinces.values():
         raise ValueError("Invalid province argument")
 
+    # changes the province to its full name if it is not already
     if provinces.get(province) != None:
         province = provinces[province]
 
+    # makes sure that the river ends with river
     if not river.endswith(" River"):
         river = river + " River"
 
@@ -26,4 +30,3 @@ def get_dam_levels(startDate, endDate, session, province, river):
     query = "SELECT * FROM CENTRAL." + table + " WHERE River = '" + river + "' AND Date >= " + startDate + " AND Date <= " + endDate
 
     return session.execute_query(query, pandas=True) 
-    
