bafd5d3b13cf80754f0203047ac8962e26c21414
kylezehaoqin <qinz@rpi.edu>
2020-07-27 18:00:03 -0700

    lwe date getter method

--- Getter Methods/grace-gracefo_getter.py
+++ Getter Methods/grace-gracefo_getter.py
@@ -1,40 +1,28 @@
 import pandas as pd
 from cssaw_central.Session import Session
 
-def get_Monthly(session):
+
+def get_lwe_byRange(startDate, endDate, session, bbox = None):
+  '''
+  @param startDate (YYYYMMDD)
+  @param endDate (YYYYMMDD)
+  @param session (cssaw_central.Session)
+  @param bbox (A list of coordinates to bound regional date [lat_max, lon_max, lat_min, lon_min])
+  Returns a pandas dataframe with land water equivalent thickness data
+  within the specified range
+  '''
 
   table = "lwe_data"
-  startMonth = input("Enter the start month as MM/YYYY:\n")
-  endMonth = input("Enter the end month as MM/YYYY:\n")
-
-  # Parsing input to help SELECT from database
-  startMonth = startMonth.split("/")
-  startMonth[1] = startMonth[1][-2:]
-  startMonth.insert(1, "1")
-  endMonth = endMonth.split("/")
-  endMonth[1] = endMonth[1][-2:]
-  endMonth.insert(1, "31")
-  startMonth = ("/").join(startMonth)
-  endMonth = ("/").join(endMonth)
-
-  if startMonth[0] == '0':
-    startMonth = startMonth[1:]
-  if endMonth[0] == '0':
-    endMonth = endMonth[1:]
-
-  print(startMonth, endMonth)
-
-  sql = open("query.sql", "w")
-  query = "SELECT * FROM CENTRAL." + table + \
-          ""
-
-
-
-  
-
-
-
-
+  query = "SELECT * FROM CENTRAL." + table \
+          + " WHERE 'Date' >= " + startDate \
+          + " AND 'Date' <= " + endDate
+  if bbox != None and len(bbox) == 4:
+    query += " AND 'Latitude' >= " + bbox[2] \
+           + " AND 'Latitude' <= " + bbox[0] \
+           + " AND 'Longitude' >= " + bbox[3] \
+           + " AND 'Longitude' <= " + bbox[1]
+  print(query)
+  return session.execute_query(query, pandas = True)
 
 
 if __name__ == "__main__":
@@ -49,5 +37,7 @@ if __name__ == "__main__":
     print("Database connection successful")
   except:
     print("Connection error")
-
-  data = get_Monthly(session)
+  
+  bbox = ['-25.5', '25.5', '-30.5', '30.5']
+  data = get_lwe_byRange("20020101", "20030101", session)
+  date_2 = get_lwe_byRange("20020101", "20030101", session, bbox)
