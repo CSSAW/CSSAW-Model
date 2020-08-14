13be4d40f6cdd8fcfbf817579245115ff7798459
Brendan <53023770+brendankhoury@users.noreply.github.com>
2020-08-13 19:51:04 -0700

    Fixed the height issue and added color differences

--- Documentation/dwa_visualization.py
+++ Documentation/dwa_visualization.py
@@ -1,6 +1,9 @@
 import pydeck
-from geojson import Feature, Point, FeatureCollection
+from geojson import Feature, Point, FeatureCollection, Polygon
 from cssaw_central.Session import Session
+
+from sklearn import preprocessing
+import pandas as pd
 # Some c
 def create_geo_json(date, sess):
 
@@ -20,18 +23,29 @@ def create_geo_json(date, sess):
     # The line below gets rid of the Day portion of the Date column,
     # it is used in the groupby to return monthly data. 
     # dataFrame["DATE"] = dataFrame["DATE"].apply(lambda x: (int)(x / 100))
-    print(dataFrame.head())
 
     dataFrame = dataFrame.groupby("station").mean()
+    normalized_data = dataFrame["COR.LEVEL"]
+    # print(normalized_df.head())
+
+    # min_max_scaler = preprocessing.MinMaxScaler()
+    # normalized_data = min_max_scaler.fit_transform(normalized_data.reshape())
+    print(pd.DataFrame(dataFrame["COR.LEVEL"]).max() - pd.DataFrame(dataFrame["COR.LEVEL"]).min() )
+    dataFrame["normalizedCor"] = pd.DataFrame(dataFrame["COR.LEVEL"]).apply(lambda x: (x-x.min())/(x.max()-x.min()))
+
     print(dataFrame.head())
-    
+    # normalized_df=(normalized_df-normalized_df.min())/(normalized_df.max()-normalized_df.min())
+    polygonSize = 0.02/2
     geoJsonList = []
+    # print(normalized_df.head())
     for index,row in dataFrame.iterrows():
-        feature = Feature(geometry=Point((row['long'], row['lat'])), properties={"elevation":row["COR.LEVEL"]*50000})
+        # feature = Feature(geometry=Point((row['long'], row['lat'])), properties={"elevation":row["COR.LEVEL"]})
+        feature = Feature(geometry=Polygon([[[row['long']-polygonSize, row['lat']-polygonSize],[row['long']-polygonSize, row['lat']+polygonSize],[row['long']+polygonSize, row['lat']+polygonSize],[row['long']+polygonSize, row['lat']-polygonSize]]]), 
+        properties={"elevation": row["COR.LEVEL"], "normalizedElevation": row["normalizedCor"]
+        })
         geoJsonList.append(feature)
 
     featureCollection = FeatureCollection(geoJsonList)
-    print(featureCollection)
     return featureCollection
 
 if __name__ == "__main__":
@@ -43,21 +57,9 @@ if __name__ == "__main__":
     credentials.close()
     
     sess = Session(username,password, host, db='Test')
-    # DATA_URL = "https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/geojson/vancouver-blocks.json"
-    data = create_geo_json(19050100,sess)
+    data = create_geo_json(19660100,sess)
     
-    LAND_COVER = [[[-123.0, 49.196], [-123.0, 49.324], [-123.306, 49.324], [-123.306, 49.196]]]
-
     INITIAL_VIEW_STATE = pydeck.ViewState(latitude=-24.654950, longitude=29.3906515, zoom=7, max_zoom=16, pitch=45, bearing=0)
-
-    # polygon = pydeck.Layer(
-    #     "PolygonLayer",
-    #     LAND_COVER,
-    #     stroked=False,
-    #     # processes the data as a flat longitude-latitude pair
-    #     get_polygon="-",
-    #     get_fill_color=[255, 0, 0, 20],
-    # )
 
     geojson = pydeck.Layer(
         "GeoJsonLayer",
@@ -67,13 +69,12 @@ if __name__ == "__main__":
         filled=True,
         extruded=True,
         wireframe=True,
-        # get_elevation='properties.corlevel * 20',
-         elevation_range=[0, 3000],
-         elevation_scale=50,
+        get_elevation="elevation*1500000",
         get_radius=1000, 
-        get_fill_color=[255, 0, 10, 255], 
+        get_fill_color="[255 ,100, normalizedElevation * 255, 255]", 
         get_line_color=[255, 255, 255],
     )
+
 
     r = pydeck.Deck(layers=[ geojson], initial_view_state=INITIAL_VIEW_STATE)
 
